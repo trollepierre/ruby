d174906a58e26c425ae7fdc52feb284a9a926ceb
Antoine BERNARD <abernard@octo.com>
2015-12-22 14:11:45 +0100

    Monnayeur dojo

---
+++ spec/monnayeur_spec.rb
@@ -0,0 +1,33 @@
+require "bundler"
+Bundler.require
+
+class Monnayeur
+  def initialize coins
+    @coins = coins.sort.reverse
+  end
+
+  def sum coins
+    coins.inject(0) { |acc, coin| acc + coin }
+  end
+
+# Implementation EauPrecieuse
+  def change amount
+    result = @coins.inject([]) do |acc, coin|
+      acc + (coin > sum(acc) ? [coin] : [])
+    end
+
+    raise "Not enough coins" if sum(result) != amount
+    result
+  end
+end
+
+RSpec.describe Monnayeur do
+  it { expect(Monnayeur.new([]).change(0)).to eq [] }
+  it { expect(Monnayeur.new([1]).change(1)).to eq [1] }
+  it { expect(Monnayeur.new([1, 1]).change(1)).to match_array [1] }
+  it { expect(Monnayeur.new([1, 1]).change(2)).to match_array [1,1] }
+  it { expect(Monnayeur.new([1, 2]).change(3)).to match_array [2,1] }
+  it { expect(Monnayeur.new([1, 2, 1]).change(2)).to match_array [2] }
+  it { expect{Monnayeur.new([1]).change(2)}.to raise_error "Not enough coins" }
+  it { expect{Monnayeur.new([2]).change(1)}.to raise_error "Not enough coins" }
+end
--- spec/some_spec.rb
+++
@@ -1,6 +0,0 @@
-require "bundler"
-Bundler.require
-
-RSpec.describe "something silly" do
-  it { expect(2 + 2).to eq 5 }
-end
